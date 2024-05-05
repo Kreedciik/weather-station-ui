6cae60985b2398ad949e5b6a68f1d2fe36fb55fd
Ilfat <ilfatislamgaliev@gmail.com>
2024-05-05 13:27:35 +0200

    Added chart

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 'use strict'
 
-const tempElement = document.querySelector('.temp');
-const humidityElement = document.querySelector('.humidity');
 const dateElement = document.querySelector('.date');
 const sensorDataWrap = document.querySelector('.sensor-data-wrap');
+const ctx = document.getElementById('myChart');
 
 class Square {
     constructor(name){
@@ -23,7 +22,11 @@ class Square {
         this.valueSelector = document.querySelector(`.${this.name}`);
 }
 
-    checkData(value){
+    checkData(value, isDisabled){
+        if(isDisabled){
+            this.removeAlert();
+            return;
+        }
         if(this.name === 'temperature'){
             if(value >= 35){
                 this.showAlert();
@@ -31,7 +34,7 @@ class Square {
                 this.removeAlert();
             }
         } else {
-            if(value >= 70){
+            if((value <= 30 || value >= 70)){
                 this.showAlert();
             } else {
                 this.removeAlert();
@@ -53,21 +56,83 @@ class Square {
     }
 }
 
+class DrawChart {
+
+    data = {
+        labels: [0],
+        datasets: [
+        {
+          label: 'Temperature °C',
+          data: [0],
+          borderWidth: 1,
+          lineTension: 0.5,
+          pointStyle: false
+        },
+        {
+          label: 'Humidity %',
+          data: [0],
+          borderWidth: 1,
+          lineTension: 0.5,
+          pointStyle: false
+        },
+    ]
+      };
+    initialize(){
+        this.liveChart = new Chart(ctx, {
+            type: 'line',
+            data: this.data,
+            options: {
+              responsive: true,
+              maintainAspectRatio: true,
+              scales: {
+                y: {
+                  beginAtZero: true,
+                  max: 100
+                }
+              }
+            }
+          });
+    }
+
+    updateChartData(temp, humidity){
+        let now = new Date();
+        now = now.getHours() + ':' + now.getMinutes();
+        if(this.data.datasets[0].data.length > 5){
+            const labels = this.data.labels;
+            const tempData = this.data.datasets[0].data;
+            const humData = this.data.datasets[1].data;
+            this.data.labels = [labels[0], ...labels.slice(2)];
+            this.data.datasets[0].data = [tempData[0], ...tempData.slice(2)];
+            this.data.datasets[1].data = [humData[0], ...humData.slice(2)];
+        }
+        this.data.labels.push(now);
+        this.data.datasets[0].data.push(temp);
+        this.data.datasets[1].data.push(humidity);
+        this.liveChart.update();
+    }
+}
+
 const tempSquare = new Square('temperature');
 const humiditySquare = new Square('humidity');
+const chart = new DrawChart();
 
 tempSquare.drawSquare();
 humiditySquare.drawSquare();
+chart.initialize();
+
 function showToday(){
     const today = new Date();
     dateElement.innerHTML = `${today.toDateString()}`
 }
 
-function showData(temp, humidity){
+function showData(temp, humidity, isDisabled){
     tempSquare.updateData(temp + '°C');
     tempSquare.checkData(temp);
     humiditySquare.updateData(humidity + '%');
-    humiditySquare.checkData(humidity);
+    humiditySquare.checkData(humidity, isDisabled);
+    if(temp && humidity){
+        chart.updateChartData(temp, humidity);
+    }
 }
 
 async function getData(){
@@ -82,8 +147,8 @@ async function getData(){
         const {data: {temperature = '', humidity = ''}} = data;
         showData(temperature, humidity);
     } catch(e){
-        showData(0, 0)
+        showData(0, 0, true)
     }
 }
 
-setInterval(getData, 500);
+setInterval(getData, 1000);
